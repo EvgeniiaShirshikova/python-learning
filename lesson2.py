9ea77913dd5724fd59811948f22409e6f6c1724b
EvgeniiaShirshikova <shirshikovae@gmail.com>
2025-09-13 16:04:17 -0400

    add lesson2

--- lesson2.py
+++ lesson2.py
@@ -1,14 +1,38 @@
+# name = "Oxana"
+# print(name)
+# print('Hi', name)
+
+# a = 2
+# b = '3'
+#
+# print(a + int(b))
+
+# a = 'Hello'
+# b = 'World'
+# message = a + " " + b
+
+# name = 'Jean'
+# age = 39
+#
+# print(f"Hi {name}! You're {age} years old.")
+
+message = "Hello World!"
+print(message.upper())
+print(message.lower())
+print(message.capitalize())
+print(message.title())
+
 # num = 100
 # name = "Алексей"
 # first_number = 1
 # my_text = "text"
 # phone_number = "Hi!"
 #
-my_name = "Jean"
+# my_name = "Jean"
 
-print("Hi, my name is " + my_name + ".")
-print("Hi, my name is", my_name, end=".\n")
-print(f"Hi, my name is {my_name}.")
+# print("Hi, my name is " + my_name + ".")
+# print("Hi, my name is", my_name, end=".\n")
+# print(f"Hi, my name is {my_name}.")
 
 # Создать переменную name, значение которой определяется через ввод данных с клавиатуры.
 # Создать переменную surname, значение которой определяется через ввод данных с клавиатуры.
@@ -16,17 +40,17 @@ print(f"Hi, my name is {my_name}.")
 # к корректному виду. Например, если с клавиатуры вводятся строки "еЛеНА" и "поПОВА", то результат должен быть
 # "Привет, меня зовут Елена Попова."
 
-name = input("Enter your name: ")
-surname = input("Enter your surname: ")
-print("Hi, my name is " + name.title() + " " + surname.title() + ".")
-print(f"Hi, my name is {name.title()} {surname.title()}.")
+# name = input("Enter your name: ")
+# surname = input("Enter your surname: ")
+# print("Hi, my name is " + name.title() + " " + surname.title() + ".")
+# print(f"Hi, my name is {name.title()} {surname.title()}.")
 
 # Создать переменную a, значение которой определяется через ввод данных с клавиатуры.
 # Создать переменную b, значение которой определяется через ввод данных с клавиатуры.
 # Вывести на экран сумму двух введенных чисел, если для a было введено 123, а для b 431,
 # то на экран должно быть выведено 554.
 
-a = int(input("Input the first number: "))
-b = int(input("Input the second number: "))
-
-print(a + b)
+# a = int(input("Input the first number: "))
+# b = int(input("Input the second number: "))
+#
+# print(a + b)
